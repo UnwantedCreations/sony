ca794440e6f8d307cab02a4b8e42bddb3941d80a
Arun <unwantedarun@hotmail.com>
2024-05-01 16:25:46 +0530

    dfgvd

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,6 +229,17 @@
 
   });
 
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
   /**
    * Initiate portfolio lightbox 
    */
